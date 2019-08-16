22dc742a27b9d9807d7896f5608e0f94d7b6ade3
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2019-08-16 11:19:57 -0700

    error message pops up if user puts invalid zipcode

---
+++ src/ErrorMessage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Popup from "reactjs-popup";
+
+
+function ErrorMessage(props) {
+
+
+    return(
+
+    <div>
+        <h1>
+            {props.message}
+        </h1>
+    </div>
+    )
+
+}
+
+export default ErrorMessage
